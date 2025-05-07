b04e257a6f9bc99b89d0b46dc22f865370c765e1
PedroGarbin <pedrogarbin13@gmail.com>
2025-05-07 20:43:02 -0300

    test: change min height

--- launch/amp_pointcloud_to_laserscan.launch.py
+++ launch/amp_pointcloud_to_laserscan.launch.py
@@ -13,20 +13,20 @@ def generate_launch_description():
             package='pointcloud_to_laserscan',
             executable='pointcloud_to_laserscan_node',
             remappings=[
-                ('cloud_in', '/lidar/Lidar1'),
+                ('cloud_in', 'fsds/lidar/Lidar2'),
                 ('scan', '/scan')
             ],
             parameters=[{
                 'target_frame': 'fsds/FSCar',
                 'transform_tolerance': 0.01,
-                'min_height': 0.1,
+                'min_height': 0.06,
                 'max_height': 1.0,
                 'angle_min': -1.5708,  # -M_PI/2
                 'angle_max': 1.5708,   # M_PI/2
-                'angle_increment': 0.0087,  # M_PI/360.0
+                'angle_increment': 0.0087/2,  # M_PI/360.0
                 'scan_time': 0.3333,
-                'range_min': 1.5,
-                'range_max': 15.0,
+                'range_min': 0.5,
+                'range_max': 20.0,
                 'use_inf': True,
                 'inf_epsilon': 1.0
             }],
